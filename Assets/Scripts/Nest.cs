c4973b8b31ff16f2638684d2c139a01ee258bf84
Raestkjoot <gunnarmagnum@gmail.com>
2026-04-11 13:12:58 +0200

    putting things in the nest

--- Assets/Scripts/Nest.cs
+++ Assets/Scripts/Nest.cs
@@ -1,7 +1,19 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Nest : MonoBehaviour
 {
+    [SerializeField] private Transform _nestPosition;
+    [SerializeField] private float _nestRadius;
+    [SerializeField] private float _nestHeight;
+
+    private void OnValidate()
+    {
+        VisualDebug.Instance.DrawSphere(_nestPosition.position, _nestRadius, Color.rebeccaPurple, 1.0f);
+        VisualDebug.Instance.DrawSphere(_nestPosition.position, _nestHeight, Color.brown, 1.0f);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -11,8 +23,10 @@ public class Nest : MonoBehaviour
             {
                 Transform thing = yoinker.TakeYoinkable();
 
+                Vector2 randomCircle = Random.insideUnitCircle * _nestRadius;
+                Vector3 nestOffsetPosition = new Vector3(randomCircle.x, _nestHeight, randomCircle.y);
+                thing.position = _nestPosition.position + nestOffsetPosition;
                 thing.parent = transform;
-                thing.localPosition = Vector3.zero;
             }
         }
     }
